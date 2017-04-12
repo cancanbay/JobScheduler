e857033d6da819204970ebb3b0e3b11487dac978
Can Canbay <canbay9942@gmail.com>
2017-04-12 19:35:57 +0300

    reminder has completed without work duration

--- app/src/main/java/com/example/canbay/jobscheduler/MyJobServiceClass.java
+++ app/src/main/java/com/example/canbay/jobscheduler/MyJobServiceClass.java
@@ -1,12 +1,17 @@
 package com.example.canbay.jobscheduler;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobScheduler;
 import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v7.app.NotificationCompat;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -20,11 +25,27 @@ public class MyJobServiceClass extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
-        
-        Toast.makeText(getApplicationContext(),"ON START JOB",Toast.LENGTH_LONG).show();
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(SendSmsActivity.etPhone.getText().toString(), null, "Hello World!", null, null);
-        jobFinished(params,false);
+
+        if(params.getJobId() ==1) {
+            Toast.makeText(getApplicationContext(), "ON START JOB WITH ID "+params.getJobId(), Toast.LENGTH_LONG).show();
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(SendSmsActivity.etPhone.getText().toString(), null, "Hello World!", null, null);
+        }
+        else if (params.getJobId() == 2){
+            Toast.makeText(getApplicationContext(), "ON START JOB WITH ID "+params.getJobId(), Toast.LENGTH_LONG).show();
+            NotificationCompat.Builder notification = new NotificationCompat.Builder(this);
+            notification.setSmallIcon(R.drawable.notification);
+            notification.setContentTitle("Click Me!");
+            notification.setContentText("Hello from Can Canbay !");
+            Intent notificationIntent = new Intent(this, ReminderActivity.class);
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            notification.setContentIntent(contentIntent);
+            // Add as notification
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, notification.build());
+        }
+        jobFinished(params, false);
         return true;
     }
 
--- app/src/main/java/com/example/canbay/jobscheduler/ReminderActivity.java
+++ app/src/main/java/com/example/canbay/jobscheduler/ReminderActivity.java
@@ -1,20 +1,80 @@
 package com.example.canbay.jobscheduler;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 /**
  * Created by canbay on 11.04.2017.
  */
 
 public class ReminderActivity extends AppCompatActivity {
+    Button btnSchedule;
+    RadioButton radioAny,radioWifi;
+    EditText etDuration;
+    JobScheduler jobScheduler;
+    JobInfo jobInfo;
+    boolean requiresCharging = false;
+    boolean requiresIdle = false;
+    int networkType;
+    CheckBox chkPlug,chkIdle;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+        btnSchedule = (Button) findViewById(R.id.btnSchedule);
+        etDuration = (EditText) findViewById(R.id.etDuration);
+        radioAny = (RadioButton) findViewById(R.id.radioAny);
+        radioWifi = (RadioButton) findViewById(R.id.radioWifi);
+        chkPlug = (CheckBox) findViewById(R.id.chkPlug);
+        chkIdle = (CheckBox) findViewById(R.id.chkIdle);
+        btnSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getParams();
+                scheduleJob();
+                finish();
+            }
+        });
+
+    }
+    private void scheduleJob(){
+        jobScheduler  = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobInfo = new JobInfo.Builder(2, new ComponentName(this, MyJobServiceClass.class))
+                .setRequiredNetworkType(networkType)
+                .setRequiresCharging(requiresCharging)
+                .setRequiresDeviceIdle(requiresIdle)
+                .build();
+        int result = jobScheduler.schedule(jobInfo);
+        if(result == JobScheduler.RESULT_SUCCESS){
+            Toast.makeText(getApplicationContext(),"Job will be scheduled with network type of "+networkType+" required charging type with "+requiresCharging+" and idle mode with" +requiresIdle,Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void getParams(){
+        if(radioAny.isChecked()){
+            networkType = JobInfo.NETWORK_TYPE_ANY;
+        }
+        else if (radioWifi.isChecked()){
+            networkType = JobInfo.NETWORK_TYPE_UNMETERED;
+        }
+
+        if(chkPlug.isChecked()){
+            requiresCharging = true;
+        }
+        if(chkIdle.isChecked()){
+            requiresIdle = true;
+        }
 
     }
 }
